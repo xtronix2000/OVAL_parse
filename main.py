b180b12ca935e72652d77f99d449932b1ec7a05a
Ainur <47248272+xtronix2000@users.noreply.github.com>
2023-10-03 16:46:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,21 +17,22 @@ for definition in root.findall('.//{http://oval.mitre.org/XMLSchema/oval-definit
     vuln['fix'] = definition.find('.//{http://oval.mitre.org/XMLSchema/oval-definitions-5}criteria/{http://oval.mitre.org/XMLSchema/oval-definitions-5}criterion').get('comment')
     vuln['cve'] = []
     
-    # Пробег по всем ссылкам, у которых ref_id содержит 'CVE'
-    for cve in definition.findall('.//{http://oval.mitre.org/XMLSchema/oval-definitions-5}metadata/{http://oval.mitre.org/XMLSchema/oval-definitions-5}reference'):
-        if 'CVE' in cve.attrib.values():
-            vuln['cve'].append(cve.attrib['ref_id'])
+    # Пробег по всем ссылкам, у которых ref_id содержит 'CVE'    
+    for item in definition.find('.//{http://oval.mitre.org/XMLSchema/oval-definitions-5}metadata/{http://oval.mitre.org/XMLSchema/oval-definitions-5}advisory'):
+        if 'cve' in item.tag:  
+           vuln['cve'].append({item.text: item.get('cvss3')[:item.get('cvss3').find('/')]})
+ 
     vuln['description'] = definition.find('.//{http://oval.mitre.org/XMLSchema/oval-definitions-5}metadata/{http://oval.mitre.org/XMLSchema/oval-definitions-5}description').text.replace('\n', '')
     # Добавление описание уязвимости в список
     vulnerabilities.append(vuln)
+ 
+# Вывод в терминал 
+#for i, dct in enumerate(vulnerabilities, start=1):
+    #!print('Vuln', i)
+    #for k, v in dct.items():
+        #print(f'{k.upper()}: {v}')
+    #print()
 
 # Преобразование в упрощенный формат
 with open('output.json', 'w') as wfile:
     json.dump(vulnerabilities, wfile, indent=4)
-
-    
-#for i, dct in enumerate(vulnerabilities, start=1):
-#    print('Vuln', i)
-#    for k, v in dct.items():
-#        print(f'{k.upper()}: {v}')
-#    print()
